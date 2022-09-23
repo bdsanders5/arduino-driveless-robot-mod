7d842a016535d0b227d3c500fc5744b52deae6a6
Bryan Sanders <bd_sanders@yahoo.com>
2022-09-23 11:39:32 -0500

    Update test-BTS7960-motor-driver.ino

--- test-BTS7960-motor-driver.ino
+++ test-BTS7960-motor-driver.ino
@@ -17,18 +17,63 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int i;
-  for(i = 0; i <= 255; i= i+10){ //clockwise rotation
-   analogWrite(R_PWM, i);
-   analogWrite(L_PWM, 0);
-   delay(500);
+  //testHardCodedSequence();
+  testFauxRcInputs();
+}
+
+void testHardCodedSequence() {
+    // put your main code here, to run repeatedly:
+    int i;
+    for(i = 0; i <= 255; i= i+10){ //clockwise rotation
+     analogWrite(R_PWM, i);
+     analogWrite(L_PWM, 0);
+     delay(500);
+    }
+    delay(500);
+    for(i = 0; i <= 255; i= i+10){ //counter clockwise rotation
+     analogWrite(R_PWM, 0);
+     analogWrite(L_PWM, i);
+     delay(500);
+    }
+    delay(500);
+}
+
+void testFauxRcInputs() {
+  
+  ///joyposVert = ch1;
+  //joyposHorz = ch2;
+  //simulate...
+  int joyposVert = 1750;
+  int joyposHorz = 1120;
+  
+  int motor1Speed = 0;
+  
+  
+  //hard coded vars...
+  int centerPositionVert = 1500;
+  int centerPositionHorz = 1500;
+  int signalBuffer = 40;
+
+  //make sure valid signal incoming..
+  if ((joyposVert > 750) && (joyposHorz > 750)) {
+    
+    //handle up position...
+    if (joyposVert > (centerPositionVert + signalBuffer)) {
+      //Determine Motor Speeds
+      motor1Speed = map(joyposVert, 1500, 2025, 0, 255); // motor 1... 0 is stopped, 255 is full reverse...
+      analogWrite(R_PWM, motor1Speed);
+      analogWrite(L_PWM, 0);
+
+    //handle down position...
+    } else if (joyposVert < (centerPositionVert - signalBuffer)) {
+      //Determine Motor Speeds
+      motor1Speed = map(joyposVert, 1500, 975, 0, 255);// motor 1... 0 is stopped, 255 full forward...
+      analogWrite(R_PWM, 0);
+      analogWrite(L_PWM, motor1Speed);
+    } else {
+      // This is Stopped
+      analogWrite(L_PWM, 0);
+      analogWrite(R_PWM, 0);
+    }
   }
-  delay(500);
-  for(i = 0; i <= 255; i= i+10){ //counter clockwise rotation
-   analogWrite(R_PWM, 0);
-   analogWrite(L_PWM, i);
-   delay(500);
-  }
-  delay(500);
 }
